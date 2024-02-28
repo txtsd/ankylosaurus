28ba54f6f1fc68f5097a03dc9af54883f0cf7129
txtsd <code@ihavea.quest>
2024-02-28 11:52:02 +0530

    chore: Update list of repos in refre.sh c[_]
    Signed-off-by: txtsd <code@ihavea.quest>

--- refre.sh
+++ refre.sh
@@ -6,15 +6,16 @@ repos=(
     blockbench-bin
     blockbench-git
     blockify-git
+    dissent-git
     espanso-git
     espanso-wayland-git
     eza-git
     fast-discord-git
     flaresolverr
     flaresolverr-bin
+    gpt-engineer
     gtkcord4
     gtkcord4-bin
-    gtkcord4-git
     illarion-bin
     imgbrd-grabber-appimage
     linux-command-gpt
@@ -31,6 +32,11 @@ repos=(
     prismlauncher-qt5-git
     progress-quest-bin
     python-baycomp
+    python-langchain
+    python-langchain-community
+    python-langchain-core
+    python-langsmith
+    python-objection
     python-pox
     python-pykakasi
     python-syncedlyrics
@@ -45,7 +51,6 @@ repos=(
     thesage
     trelby
     trelby-git
-    ttf-monocraft
     ttf-mutant-emoji
     ttf-unifont
     vimv-git
