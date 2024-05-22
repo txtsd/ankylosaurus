0079c66fa793f4c3ec21c83e27a8faa4b3e672d6
txtsd <code@ihavea.quest>
2024-05-22 07:54:43 +0530

    Add push script ¯\_(ツ)_/¯
    Signed-off-by: txtsd <code@ihavea.quest>

---
+++ pu.sh
@@ -0,0 +1,70 @@
+repos=(
+    archosaur
+    archosaur-bin
+    archosaur-git
+    blockbench
+    blockbench-bin
+    blockbench-git
+    blockify-git
+    dissent
+    dissent-bin
+    dissent-git
+    dpitunnel
+    dpitunnel-bin
+    dpitunnel-git
+    espanso-git
+    eza-git
+    fast-discord-git
+    flaresolverr
+    flaresolverr-bin
+    gpt-engineer
+    halloy
+    halloy-bin
+    halloy-git
+    illarion-bin
+    imgbrd-grabber-appimage
+    java-openjdk-wakefield
+    linux-command-gpt
+    linux-command-gpt-git
+    openssh-askpass
+    otf-monocraft
+    otf-unifont
+    packwiz-git
+    prismlauncher
+    prismlauncher-bin
+    prismlauncher-git
+    prismlauncher-qt5
+    prismlauncher-qt5-bin
+    prismlauncher-qt5-git
+    progress-quest-bin
+    python-baycomp
+    python-langchain
+    python-langchain-community
+    python-langchain-core
+    python-langchain-text-splitters
+    python-langsmith
+    python-pox
+    python-pykakasi
+    python-syncedlyrics
+    ripcord
+    ripcord-arch-libs
+    ripme
+    ripme-bin
+    ripme-git
+    soundsense
+    soundsense-rs-git
+    stevenarella-git
+    thesage
+    trelby
+    trelby-git
+    ttf-unifont
+    vimv-git
+    yt-spammer-purge
+    zaread-git
+)
+
+for repo in "${repos[@]}"; do
+    echo ''
+    echo "--> Pushing ${repo}"
+    archosaur "${repo}" --speedup || exit 1
+done
